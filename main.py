5b4310bc4d0612e1bfd555c8870feac8bfeb6d68
Amartya pandey <60804993+AMARTYA2020@users.noreply.github.com>
2020-12-02 07:22:23 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,14 @@
+import time
+from plyer import notification
+
+if __name__=="__main__":
+    while True:
+        notification.notify(
+            title = "Please drink water",
+            message="Fluids and hydration are crucial for the bodily functions of humans, especially when it comes to drinking water. As you probably remember, 60-80 percent of the human body is water. ",
+            app_icon=r"C:\Users\AMARTYA PANDEY\PycharmProjects\Drink_water_Notification\icon.ico",
+            timeout=10
+        )
+        time.sleep(60*60)
+
+
